4222a8d31fb41d1e8ab2838120cf649154e2a663
Alexey Svirchevsky <svr.work@protonmail.com>
2020-02-05 12:08:26 +0300

    5.1 broken

--- Ch5.thy
+++ Ch5.thy
@@ -27,4 +27,19 @@ assume "surj f"
   thus "False" by blast
 qed
 
-end
+(* 5.1 *)
+
+lemma assumes T : "\<forall> x y. T x y \<or> T y x"
+  and A:  "\<forall> x y. A x y \<and> A y x \<longrightarrow> x = y"
+  and TA: "\<forall> x y. T x y \<longrightarrow> A x y"
+  and A2: "A x y"
+  shows "T x y"
+proof -
+  assume "\<not> T x y"
+  from T and this have "T y x" by auto
+  from TA and this have "A y x" by auto
+  from this and A2 have "A x y \<and> A y x" by auto
+  from this and A have "x = y" by auto
+  from this and T have "T x y" by auto
+  from this and `\<not> T x y` show "False" by auto
+qed
